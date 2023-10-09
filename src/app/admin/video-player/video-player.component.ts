119e5b9a707733248b31b949786d2a0b65787794
Yashraj Dawkhar <dawkharyashraj@gmail.com>
2023-10-10 00:58:17 +0530

    update

---
+++ src/app/admin/video-player/video-player.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-video-player',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.scss']
+})
+export class VideoPlayerComponent {
+
+}
